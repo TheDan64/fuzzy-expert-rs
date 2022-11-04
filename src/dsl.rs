5960da9c394ff058531eb1e395ece30c68014342
Daniel Kolsoi <ThaDan64@gmail.com>
2022-11-03 23:00:41 -0400

    Added generic rules

--- src/dsl.rs
+++ src/dsl.rs
@@ -1,28 +1,27 @@
 use crate::Variable;
 
-pub enum Expr {
-    // REVIEW: Maybe fixed_map crate for RHS? Could be Key or Value? Or Just an enum?
-    Eq(Variable, &'static str),
-    And(Vec<Expr>),
-    Or(Vec<Expr>),
+pub enum Expr<T> {
+    Eq(Variable, T),
+    And(Vec<Expr<T>>),
+    Or(Vec<Expr<T>>),
 }
 
-impl Expr {
-    pub fn or(self, rhs: Expr) -> Self {
+impl<T> Expr<T> {
+    pub fn or(self, rhs: Expr<T>) -> Self {
         Expr::Or(vec![self, rhs])
     }
 
-    pub fn and(self, rhs: Expr) -> Self {
+    pub fn and(self, rhs: Expr<T>) -> Self {
         Expr::And(vec![self, rhs])
     }
 
-    pub fn and2(self, rhs: Expr, rhs2: Expr) -> Self {
+    pub fn and2(self, rhs: Expr<T>, rhs2: Expr<T>) -> Self {
         Expr::And(vec![self, rhs, rhs2])
     }
 }
 
 impl Variable {
-    pub fn eq(self, rhs: &'static str) -> Expr {
-        Expr::Eq(self, rhs)
+    pub fn eq<T>(self, rhs: impl Into<T>) -> Expr<T> {
+        Expr::Eq(self, rhs.into())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -44,14 +44,14 @@ struct VariableContraints {
 }
 
 #[derive(Default)]
-pub struct Rules(Vec<Rule>);
+pub struct Rules<T>(Vec<Rule<T>>);
 
-impl Rules {
+impl<T: Terms> Rules<T> {
     pub fn new() -> Self {
         Rules(Vec::new())
     }
 
-    pub fn add(&mut self, premise: Expr) {
+    pub fn add(&mut self, premise: Expr<T>) {
         self.0.push(Rule {
             premise,
             consequence: (),
@@ -59,12 +59,16 @@ impl Rules {
     }
 }
 
-struct Rule {
-    premise: Expr,
+struct Rule<T> {
+    premise: Expr<T>,
     consequence: (),
 }
 
-fn eval(vars: &Variables, rules: &Rules, input: &[()]) {}
+trait Terms {
+    fn values(&self) -> &'static [(f32, f32)];
+}
+
+fn eval<T: Terms>(vars: &Variables, rules: &Rules<T>, input: &[()]) {}
 
 #[test]
 fn test_bank_loan() {
@@ -92,14 +96,81 @@ fn test_bank_loan() {
         "Goodc" => vec![(2., 1.), (3., 0.7), (4., 0.3), (5., 0.)],
         "Badc" => vec![(5., 0.), (6., 0.3), (7., 0.7), (8., 1.)],
     };
+
+    enum Score {
+        High,
+        Low,
+    }
+
+    enum Ratio {
+        Good,
+        Bad,
+    }
+
+    enum Credit {
+        Good,
+        Bad,
+    }
+
+    enum VarTerms {
+        Score(Score),
+        Ratio(Ratio),
+        Credit(Credit),
+    }
+
+    impl From<Score> for VarTerms {
+        fn from(s: Score) -> Self {
+            Self::Score(s)
+        }
+    }
+
+    impl From<Ratio> for VarTerms {
+        fn from(r: Ratio) -> Self {
+            Self::Ratio(r)
+        }
+    }
+
+    impl From<Credit> for VarTerms {
+        fn from(c: Credit) -> Self {
+            Self::Credit(c)
+        }
+    }
+
+    impl Terms for VarTerms {
+        fn values(&self) -> &'static [(f32, f32)] {
+            match self {
+                Self::Score(Score::High) => &[(175., 0.), (180., 0.2), (185., 0.7), (190., 1.)],
+                Self::Score(Score::Low) => &[
+                    (155., 1.),
+                    (160., 0.8),
+                    (165., 0.5),
+                    (170., 0.2),
+                    (175., 0.),
+                ],
+                Self::Ratio(Ratio::Good) => &[(0.3, 1.), (0.4, 0.7), (0.41, 0.3), (0.42, 0.)],
+                Self::Ratio(Ratio::Bad) => &[(0.44, 0.), (0.45, 0.3), (0.5, 0.7), (0.7, 1.)],
+                Self::Credit(Credit::Good) => &[(2., 1.), (3., 0.7), (4., 0.3), (5., 0.)],
+                Self::Credit(Credit::Bad) => &[(5., 0.), (6., 0.3), (7., 0.7), (8., 1.)],
+            }
+        }
+    }
+
     let mut vars = Variables::new();
     let score = vars.add(150. ..200., score_terms, ZeroOne(1.));
     let ratio = vars.add(0.1..1., ratio_terms, ZeroOne(1.));
     let credit = vars.add(0. ..10., credit_terms, ZeroOne(1.));
-    let mut rules = Rules::new();
+    let mut rules = Rules::<VarTerms>::new();
 
-    rules.add(score.eq("High").and2(ratio.eq("Goodr"), credit.eq("Goodc")));
-    rules.add(score.eq("Low").and(ratio.eq("Badr").or(credit.eq("Badc"))));
+    rules.add(
+        score
+            .eq(Score::High)
+            .and2(ratio.eq(Ratio::Good), credit.eq(Credit::Good)),
+    );
+    rules.add(
+        score
+            .eq(Score::Low)
+            .and(ratio.eq(Ratio::Bad).or(credit.eq(Credit::Bad))),
+    );
 
     eval(&vars, &rules, &[]);
 }
